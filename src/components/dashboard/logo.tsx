0bc0f0dd58e390fbcb031abebffd8f2d2cf8abdd
TAKIS MALAM <de.madjor.ryo@gmail.com>
2025-09-19 06:16:44 +0000

    logo F&B ada gak ya? (_for element path_)

--- src/components/dashboard/logo.tsx
+++ src/components/dashboard/logo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ShoppingCart } from 'lucide-react';
+import { UtensilsCrossed } from 'lucide-react';
 
 
 type LogoProps = {
@@ -10,7 +10,7 @@ export function Logo({ storeName }: LogoProps) {
   return (
     <div className="flex items-center gap-2 text-primary">
       <div className="flex items-center justify-center rounded-md bg-primary/20 p-2">
-        <ShoppingCart className="h-6 w-6" />
+        <UtensilsCrossed className="h-6 w-6" />
       </div>
       <div className="flex flex-col group-data-[collapsible=icon]:hidden">
         <span className="font-headline text-2xl leading-none tracking-wider text-sidebar-foreground">Chika POS F&B</span>
