261d20f4d1254c33362b58b07af7a9f4704d328a
TAKIS MALAM <de.madjor.ryo@gmail.com>
2025-09-20 09:26:27 +0000

    bingkai block seperti icon yang ada di halam login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function WelcomePage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
        <div className="flex flex-col items-center gap-4 text-center">
-            <div className="rounded-lg border-4 border-primary p-4">
+            <div className="flex items-center justify-center rounded-lg bg-primary/20 p-4">
                 <UtensilsCrossed className="h-20 w-20 animate-pulse-slow text-primary" />
             </div>
             <h1 className="font-headline text-3xl font-bold tracking-wider text-foreground">
