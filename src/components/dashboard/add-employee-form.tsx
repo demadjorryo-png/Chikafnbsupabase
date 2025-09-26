3ba8c8d48ce6f9235e1da926aa324a5c6a4ce8b9
TAKIS MALAM <de.madjor.ryo@gmail.com>
2025-09-26 20:01:56 +0000

    Try fixing this error: `Runtime Error: Error: Module [project]/node_modu

--- src/components/dashboard/add-employee-form.tsx
+++ src/components/dashboard/add-employee-form.tsx
@@ -181,7 +181,7 @@ export function AddEmployeeForm({ setDialogOpen, onEmployeeAdded }: AddEmployeeF
                     className="absolute right-1 top-1/2 -translate-y-1/2 h-7 w-7 text-muted-foreground"
                     onClick={() => setShowPassword(!showPassword)}
                 >
-                    {showPassword ? <EyeOff /> : <Eye />}
+                    {showPassword ? <EyeOff className="h-4 w-4" /> : <Eye className="h-4 w-4" />}
                 </Button>
               </div>
               <FormMessage />
