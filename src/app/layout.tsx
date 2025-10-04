f2a373caa9cca61aaa458f5e6fff79d6651cbcf7
TAKIS MALAM <de.madjor.ryo@gmail.com>
2025-10-04 07:08:08 +0000

    oh iya tambahkan tema terang ya. jadi biar bisa pilih

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
