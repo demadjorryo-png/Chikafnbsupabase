d117d8e10eb68ebfbe3c00266127bcec10ae95e5
TAKIS MALAM <de.madjor.ryo@gmail.com>
2025-10-04 16:28:53 +0000

    Changes before Firebase Studio auto-run

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,4 @@
-'use client';
+''''use client';
 
 import {initializeApp, getApps, getApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
@@ -7,12 +7,12 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyCZuXPnpa4lqpisO7U-6th_8kH_YCmgQok",
+  authDomain: "kasir-pos-chika-v2-09828-9a2a7.firebaseapp.com",
+  projectId: "kasir-pos-chika-v2-09828-9a2a7",
+  storageBucket: "kasir-pos-chika-v2-09828-9a2a7.firebasestorage.app",
+  messagingSenderId: "1041744002845",
+  appId: "1:1041744002845:web:7517aacfc7d8a8a6132bf4"
 };
 
 
@@ -22,4 +22,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { app, auth, db, storage };
+export { app, auth, db, storage };'''
